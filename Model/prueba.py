73240aaf121b3fde7cd3adaa4ae86882d243a969
Miguel Bernal <mb123mb1@hotmail.com>
2023-06-09 01:12:32 -0400

    cambio de nomre de carpetas

--- Model/prueba.py
+++ Model/prueba.py
@@ -1,8 +1,13 @@
-from io import open
-
-archivoGrafo = open("Model/grafos.txt","w")
-
-#lista = archivoGrafo.readlines()
+from GrafoNoDirigido import *
 
 
+def buildGrafo(graph):
+    g = graph()
 
+    for v in ('s', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'x'):
+        g.addVertex(Vertexx(v))
+    g.addArist(Arist(g.getVertex('s'), g.getVertex('a')))
+    return g
+
+
+g1 = buildGrafo(Grafo)
--- View/prueba.py
+++ View/prueba.py
@@ -1,13 +1,8 @@
-from GrafoNoDirigido import *
+from io import open
+
+archivoGrafo = open("Model/grafos.txt","w")
+
+#lista = archivoGrafo.readlines()
 
 
-def buildGrafo(graph):
-    g = graph()
 
-    for v in ('s', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'x'):
-        g.addVertex(Vertexx(v))
-    g.addArist(Arist(g.getVertex('s'), g.getVertex('a')))
-    return g
-
-
-g1 = buildGrafo(Grafo)
