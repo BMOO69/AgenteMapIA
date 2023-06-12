2141c476ba4d38bb3e13c9ebc7678a3c025d2597
Miguel Bernal <mb123mb1@hotmail.com>
2023-06-12 17:25:57 -0400

    funciona hasta la imagen creare una nueva rama para probar posibles pruebas

--- View/app.py
+++ View/app.py
@@ -1,5 +1,7 @@
 import customtkinter as ctk
 import tkinter as tk
+from PIL import ImageTk, Image
+
 
 from View.controller import Controller
 
@@ -23,9 +25,16 @@ class AppWindow(ctk.CTk):
         self.options_frame = ctk.CTkFrame(self.main_frame, width=1200,height=650)
         self.options_frame.grid(row=0, column=0, sticky=tk.NSEW, ipadx=10, ipady=10, padx=(0, 10), pady=(0,10))
         self.options_frame.columnconfigure(0,weight=1)
+        self.ima = Image.open("mapa.png")
+        self.nuevaima = self.ima.resize((1200,650))
 
+        self.gf = ImageTk.PhotoImage(self.nuevaima)
+        #imagen_redimensionada = self.gf.resize((1200, 650))
         self.img_frame = ctk.CTkCanvas(self.main_frame, width=1200, height=650, bg="#0E6063")
         self.img_frame.grid(row=0, column=1, sticky=tk.NSEW, ipadx=10, ipady=10, padx=(0,10))
+        self.img_frame.create_image(10, 10, image=self.gf, anchor=tk.NW)
+
+        #canvas.bind("<Button-1>", obtener_coordenadas)
         """
         self.map_label = ctk.CTkLabel(self.img_frame, font=ctk.CTkFont(size=20, weight="bold"))
          
@@ -50,7 +59,6 @@ aqui estarara el mapa"""
         self.send_agent_button.grid(row=5, column=0, sticky=tk.NSEW)
 
         self.main_frame.pack(expand=True, fill=tk.BOTH, padx=10, pady=10)
-
 
     def clean_roads(self):
         pass
